7bb91915abbc216808256c944485e14314714ec4
Ygor Carmo <ygor.carmo@gmail.com>
2024-01-17 15:07:47 +1100

    embeded files and found new way of using templates

--- main.go
+++ main.go
@@ -4,19 +4,32 @@ import (
 	"embed"
 	"fmt"
 	"html/template"
-	"log"
+	"io/fs"
 	"net/http"
 )
 
-//go:embed templates/*.tmpl
-var views embed.FS
+//go:embed templates sample
+var assets embed.FS
+
+var tmpl map[string]*template.Template
+
+func init() {
+	loadTemplates()
+}
 
 func main() {
 	server := http.NewServeMux()
 
-	fs := http.FileServer(http.Dir("./sample/gerow"))
+	// only serve the front end files
+	folder, fserr := fs.Sub(assets, "sample/gerow")
 
+	if fserr != nil {
+		fmt.Println(fserr)
+	}
+
+	fs := http.FileServer(http.FS(folder))
 	server.Handle("/", fs)
+
 	server.HandleFunc("/home", homeHandler)
 
 	err := http.ListenAndServe(":3000", server)
@@ -24,14 +37,25 @@ func main() {
 	if err != nil {
 		fmt.Println("Error while starting the server")
 	}
-
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 
-	tmpl, err := template.ParseFS(views, "**/*.tmpl")
+	fmt.Println(tmpl)
+
+	tmpl["new-home"].ExecuteTemplate(w, "base-layout.tmpl", nil)
+}
+
+func loadTemplates() {
+	tmpl = make(map[string]*template.Template)
+
+	templateFolder, err := fs.Sub(assets, "templates")
+
 	if err != nil {
-		log.Fatal(err)
+		panic(err)
 	}
-	err = tmpl.ExecuteTemplate(w, "home.tmpl", nil)
+
+	tmpl["new-home"] = template.Must(template.ParseFS(templateFolder, "base-layout.tmpl", "new-home.tmpl"))
+	tmpl["custom"] = template.Must(template.ParseFS(templateFolder, "base-layout.tmpl", "custom.tmpl"))
+	tmpl["nothing"] = template.Must(template.ParseFS(templateFolder, "base-layout.tmpl", "nothing.tmpl"))
 }
