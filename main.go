f37e95183b3d3ef61d9d62e2e98d0f8cbd7fed80
Ygor Carmo <ygor.carmo@gmail.com>
2024-02-19 11:31:52 +1100

    move assets folder and add more pages

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 	"net/http"
 )
 
-//go:embed templates sample
+//go:embed templates assets
 var assets embed.FS
 
 var tmpl map[string]*template.Template
@@ -21,17 +21,19 @@ func main() {
 	server := http.NewServeMux()
 
 	// only serve the front end files
-	folder, fserr := fs.Sub(assets, "sample/gerow")
+	folder, fserr := fs.Sub(assets, "assets")
 
 	if fserr != nil {
 		fmt.Println(fserr)
 	}
 
 	fs := http.FileServer(http.FS(folder))
-	server.Handle("/", fs)
+	server.Handle("/assets/", http.StripPrefix("/assets/", fs))
 
-	server.HandleFunc("/home", homeHandler)
+	server.HandleFunc("/", homeHandler)
 	server.HandleFunc("/about-us", aboutHandler)
+	server.HandleFunc("/services", servicesHandler)
+	server.HandleFunc("/contact", contactHandler)
 
 	err := http.ListenAndServe(":3000", server)
 
@@ -41,11 +43,25 @@ func main() {
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+		tmpl["not-found"].ExecuteTemplate(w, "base-layout.tmpl", nil)
+		return
+	}
+
 	tmpl["home"].ExecuteTemplate(w, "base-layout.tmpl", nil)
 }
 
 func aboutHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl["about"].ExecuteTemplate(w, "base-layout.tmpl", nil)
+}
+
+func servicesHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl["services"].ExecuteTemplate(w, "base-layout.tmpl", nil)
+}
+
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl["contact"].ExecuteTemplate(w, "base-layout.tmpl", nil)
 }
 
 func loadTemplates() {
@@ -60,5 +76,8 @@ func loadTemplates() {
 	tmpl["home"] = template.Must(template.ParseFS(templateFolder, "base-layout.tmpl", "home.tmpl"))
 	tmpl["custom"] = template.Must(template.ParseFS(templateFolder, "base-layout.tmpl", "custom.tmpl"))
 	tmpl["about"] = template.Must(template.ParseFS(templateFolder, "base-layout.tmpl", "about.tmpl"))
+	tmpl["services"] = template.Must(template.ParseFS(templateFolder, "base-layout.tmpl", "services.tmpl"))
+	tmpl["contact"] = template.Must(template.ParseFS(templateFolder, "base-layout.tmpl", "contact.tmpl"))
+	tmpl["not-found"] = template.Must(template.ParseFS(templateFolder, "base-layout.tmpl", "not-found.tmpl"))
 
 }
